a1b9d35592ab6bb28911171265121fec1c463c1d
Agustin <agutorres@frba.utn.edu.ar>
2024-06-24 14:02:06 -0300

    DolarHoy Flake 8 and mypy

--- dolarhoy/__init__.py
+++ dolarhoy/__init__.py
@@ -70,6 +70,7 @@ from enum import Enum
 import requests
 from bs4 import BeautifulSoup
 
+
 class TipoDolar(Enum):
     BLUE = "dolar_blue"
     OFICIAL = "dolar_oficial"
@@ -77,22 +78,25 @@ class TipoDolar(Enum):
     CONTADO_CON_LIQUI = "contado_con_liqui"
     TARJETA = "dolar_tarjeta"
 
+
 class TipoOperacion(Enum):
     COMPRA = "compra"
     VENTA = "venta"
+
+
 class DolarHoy:
     def __init__(self, url: str = "https://www.dolarhoy.com"):
         self.url = url
 
     def _scrape_data(self):
         try:
-            response = requests.get(self.url)    
+            response = requests.get(self.url)
             response.raise_for_status()
         except requests.RequestException as e:
             print(f"Error making request: {e}")
-        
-        soup = BeautifulSoup(response.content, 'html.parser')
-            
+
+        soup = BeautifulSoup(response.content, "html.parser")
+
         data = {}
 
         cotizaciones = {
@@ -100,39 +104,39 @@ class DolarHoy:
             TipoDolar.OFICIAL: "/cotizaciondolaroficial",
             TipoDolar.BOLSA: "/cotizaciondolarbolsa",
             TipoDolar.CONTADO_CON_LIQUI: "/cotizaciondolarcontadoconliqui",
-            TipoDolar.TARJETA: "/cotizacion-dolar-tarjeta"
+            TipoDolar.TARJETA: "/cotizacion-dolar-tarjeta",
         }
-        
+
         for tipo_dolar, href in cotizaciones.items():
             data[tipo_dolar.value] = self._extract_cotizacion(soup, href)
 
         return data
-    
+
     def _extract_cotizacion(self, soup, href):
-        element = soup.find('a', href=href)
+        element = soup.find("a", href=href)
         if not element:
             print(f"No element found for href {href}")
             return {}
 
-        parent = element.find_parent('div', class_='tile is-child')
+        parent = element.find_parent("div", class_="tile is-child")
         if not parent:
             print(f"No parent found for href {href} with class 'tile is-child'")
             return {}
 
-        compra = self._get_value(parent, 'compra')
-        venta = self._get_value(parent, 'venta')
+        compra = self._get_value(parent, "compra")
+        venta = self._get_value(parent, "venta")
 
         return {
             "compra": compra,
             "venta": venta,
             "source": "DolarHoy.com ®",
-            "source_url": self.url
+            "source_url": self.url,
         }
 
     def _get_value(self, parent, class_name):
-        element = parent.find('div', class_=class_name)
-        if element and element.find('div', class_='val'):
-            value = element.find('div', class_='val').text.strip('$').replace(',', '')
+        element = parent.find("div", class_=class_name)
+        if element and element.find("div", class_="val"):
+            value = element.find("div", class_="val").text.strip("$").replace(",", "")
             try:
                 return float(value)
             except ValueError:
@@ -143,36 +147,42 @@ class DolarHoy:
         return self._scrape_data()
 
     def get_blue_price(self) -> dict:
-        return self._scrape_data().get('dolar_blue')
+        return self._scrape_data().get("dolar_blue")
 
     def get_oficial_price(self) -> dict:
-        return self._scrape_data().get('dolar_oficial')
+        return self._scrape_data().get("dolar_oficial")
 
     def get_bolsa_price(self) -> dict:
-        return self._scrape_data().get('dolar_bolsa')
+        return self._scrape_data().get("dolar_bolsa")
 
     def get_contado_con_liqui_price(self) -> dict:
-        return self._scrape_data().get('contado_con_liqui')
+        return self._scrape_data().get("contado_con_liqui")
 
     def get_tarjeta_price(self) -> dict:
-        return self._scrape_data().get('dolar_tarjeta')
+        return self._scrape_data().get("dolar_tarjeta")
 
     def get_price(self, tipo_dolar: TipoDolar, tipo_operacion: TipoOperacion) -> float:
         data = self._scrape_data()
         return data.get(tipo_dolar.value, {}).get(tipo_operacion.value)
-    
+
     def get_all_prices(self, tipo_operacion: TipoOperacion) -> dict:
         data = self._scrape_data()
-        prices = {key: value.get(tipo_operacion.value) for key, value in data.items() if tipo_operacion.value in value}
+        prices = {
+            key: value.get(tipo_operacion.value)
+            for key, value in data.items()
+            if tipo_operacion.value in value
+        }
         return prices
 
     def get_summary(self) -> str:
         data = self._scrape_data()
         summary = []
         for key, value in data.items():
-            tipo_dolar = key.replace('_', ' ').capitalize()
-            compra = value.get('compra', 'N/A')
-            venta = value.get('venta', 'N/A')
-            summary.append(f"{tipo_dolar}:\nCompra: ${compra}\nVenta: ${venta}\nSource: {value.get('source')}\nSource URL: {value.get('source_url')}")
-            
+            tipo_dolar = key.replace("_", " ").capitalize()
+            compra = value.get("compra", "N/A")
+            venta = value.get("venta", "N/A")
+            summary.append(
+                f"{tipo_dolar}:\nCompra: ${compra}\nVenta: ${venta}\nSource: {value.get('source')}\nSource URL: {value.get('source_url')}"
+            )
+
         return "\n".join(summary)
--- tests/test_dolarhoy.py
+++ tests/test_dolarhoy.py
@@ -4,15 +4,21 @@ from dolarhoy import DolarHoy, TipoDolar, TipoOperacion
 # URL a utilizar
 url = "https://www.dolarhoy.com/"
 
+
 @pytest.fixture
 def dolar_hoy():
     return DolarHoy(url)
+
 
 def test_get_all_data(dolar_hoy):
     data = dolar_hoy.get_all_data()
 
     expected_keys = {
-        "dolar_blue", "dolar_oficial", "dolar_bolsa", "contado_con_liqui", "dolar_tarjeta"
+        "dolar_blue",
+        "dolar_oficial",
+        "dolar_bolsa",
+        "contado_con_liqui",
+        "dolar_tarjeta",
     }
 
     for key in expected_keys:
@@ -20,74 +26,100 @@ def test_get_all_data(dolar_hoy):
         assert isinstance(data[key], dict), f"Value for {key} should be a dict"
 
         if key == "dolar_tarjeta":
-            if 'compra' in data[key]:
-                assert data[key]['compra'] is None, f"{key} 'compra' should be None"
+            if "compra" in data[key]:
+                assert data[key]["compra"] is None, f"{key} 'compra' should be None"
         else:
-            if 'compra' in data[key]:
-                assert isinstance(data[key]['compra'], float), f"compra for {key} should be a float"
+            if "compra" in data[key]:
+                assert isinstance(
+                    data[key]["compra"], float
+                ), f"compra for {key} should be a float"
 
-        if 'venta' in data[key]:
-            assert isinstance(data[key]['venta'], float), f"venta for {key} should be a float"
+        if "venta" in data[key]:
+            assert isinstance(
+                data[key]["venta"], float
+            ), f"venta for {key} should be a float"
 
 
 def test_get_blue_price(dolar_hoy):
     price = dolar_hoy.get_blue_price()
     if price is not None:
-        assert 'compra' in price or 'venta' in price, "Both 'compra' and 'venta' keys should be in the data"
-        if 'compra' in price:
-            assert isinstance(price['compra'], float), "'compra' should be a float"
-        if 'venta' in price:
-            assert isinstance(price['venta'], float), "'venta' should be a float"
+        assert (
+            "compra" in price or "venta" in price
+        ), "Both 'compra' and 'venta' keys should be in the data"
+        if "compra" in price:
+            assert isinstance(price["compra"], float), "'compra' should be a float"
+        if "venta" in price:
+            assert isinstance(price["venta"], float), "'venta' should be a float"
+
 
 def test_get_oficial_price(dolar_hoy):
     price = dolar_hoy.get_oficial_price()
     if price is not None:
-        assert 'compra' in price or 'venta' in price, "Both 'compra' and 'venta' keys should be in the data"
-        if 'compra' in price:
-            assert isinstance(price['compra'], float), "'compra' should be a float"
-        if 'venta' in price:
-            assert isinstance(price['venta'], float), "'venta' should be a float"
+        assert (
+            "compra" in price or "venta" in price
+        ), "Both 'compra' and 'venta' keys should be in the data"
+        if "compra" in price:
+            assert isinstance(price["compra"], float), "'compra' should be a float"
+        if "venta" in price:
+            assert isinstance(price["venta"], float), "'venta' should be a float"
+
 
 def test_get_bolsa_price(dolar_hoy):
     price = dolar_hoy.get_bolsa_price()
     if price is not None:
-        assert 'compra' in price or 'venta' in price, "Both 'compra' and 'venta' keys should be in the data"
-        if 'compra' in price:
-            assert isinstance(price['compra'], float), "'compra' should be a float"
-        if 'venta' in price:
-            assert isinstance(price['venta'], float), "'venta' should be a float"
+        assert (
+            "compra" in price or "venta" in price
+        ), "Both 'compra' and 'venta' keys should be in the data"
+        if "compra" in price:
+            assert isinstance(price["compra"], float), "'compra' should be a float"
+        if "venta" in price:
+            assert isinstance(price["venta"], float), "'venta' should be a float"
+
 
 def test_get_contado_con_liqui_price(dolar_hoy):
     price = dolar_hoy.get_contado_con_liqui_price()
     if price is not None:
-        assert 'compra' in price or 'venta' in price, "Both 'compra' and 'venta' keys should be in the data"
-        if 'compra' in price:
-            assert isinstance(price['compra'], float), "'compra' should be a float"
-        if 'venta' in price:
-            assert isinstance(price['venta'], float), "'venta' should be a float"
+        assert (
+            "compra" in price or "venta" in price
+        ), "Both 'compra' and 'venta' keys should be in the data"
+        if "compra" in price:
+            assert isinstance(price["compra"], float), "'compra' should be a float"
+        if "venta" in price:
+            assert isinstance(price["venta"], float), "'venta' should be a float"
+
 
 def test_get_tarjeta_price(dolar_hoy):
     price = dolar_hoy.get_tarjeta_price()
     if price is not None:
-        assert 'venta' in price, "'venta' key should be in the data"
-        if 'venta' in price:
-            assert isinstance(price['venta'], float), "'venta' should be a float"
+        assert "venta" in price, "'venta' key should be in the data"
+        if "venta" in price:
+            assert isinstance(price["venta"], float), "'venta' should be a float"
+
 
 def test_get_price(dolar_hoy):
     for tipo_dolar in TipoDolar:
         for tipo_operacion in TipoOperacion:
             price = dolar_hoy.get_price(tipo_dolar, tipo_operacion)
             if price is not None:
-                assert isinstance(price, float), f"{tipo_operacion.value} for {tipo_dolar.value} should be a float"
+                assert isinstance(
+                    price, float
+                ), f"{tipo_operacion.value} for {tipo_dolar.value} should be a float"
+
 
 def test_get_all_prices(dolar_hoy):
     for tipo_operacion in TipoOperacion:
         prices = dolar_hoy.get_all_prices(tipo_operacion)
         for key, value in prices.items():
-            if key == TipoDolar.TARJETA.value and tipo_operacion == TipoOperacion.COMPRA:
+            if (
+                key == TipoDolar.TARJETA.value
+                and tipo_operacion == TipoOperacion.COMPRA
+            ):
                 assert value is None, f"{tipo_operacion} for {key} should be None"
             else:
-                assert isinstance(value, float), f"{tipo_operacion} for {key} should be a float"
+                assert isinstance(
+                    value, float
+                ), f"{tipo_operacion} for {key} should be a float"
+
 
 def test_get_summary(dolar_hoy):
     summary = dolar_hoy.get_summary()
