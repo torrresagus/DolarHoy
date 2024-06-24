a0e4e8a81eae4efed7dd11504931b5a5725402eb
Agustin <agutorres@frba.utn.edu.ar>
2024-06-24 11:58:40 -0300

    DolarHoy docstring

--- dolarhoy/__init__.py
+++ dolarhoy/__init__.py
@@ -1,3 +1,71 @@
+"""
+# DolarHoy
+
+`DolarHoy` es una librería en Python que permite obtener las cotizaciones de diferentes tipos de dólar en Argentina desde el sitio web [DolarHoy](https://www.dolarhoy.com/). Utiliza las librerías `requests` y `BeautifulSoup` para hacer web scraping y extraer la información relevante.
+
+## Clases Enum
+
+### `TipoDolar`
+Una enumeración que representa los diferentes tipos de dólares:
+- `BLUE`: "dolar_blue"
+- `OFICIAL`: "dolar_oficial"
+- `BOLSA`: "dolar_bolsa"
+- `CONTADO_CON_LIQUI`: "contado_con_liqui"
+- `TARJETA`: "dolar_tarjeta"
+
+### `TipoOperacion`
+Una enumeración que representa el tipo de operación:
+- `COMPRA`: "compra"
+- `VENTA`: "venta"
+
+## Clase `DolarHoy`
+
+### Constructor
+
+#### `__init__(self, url: str = "https://www.dolarhoy.com")`
+Inicializa una instancia de `DolarHoy` con la URL especificada. Por defecto, la URL es "https://www.dolarhoy.com".
+
+### Métodos Privados
+
+#### `_scrape_data(self)`
+Extrae los datos de la URL y devuelve un diccionario que contiene las cotizaciones de diferentes tipos de dólares.
+
+#### `_extract_cotizacion(self, soup, href)`
+Extrae las cotizaciones (compra y venta) para un tipo específico de dólar del objeto BeautifulSoup y la URL proporcionada.
+
+#### `_get_value(self, parent, class_name)`
+Obtiene el valor de compra o venta del elemento HTML correspondiente.
+
+### Métodos Públicos
+
+#### `get_all_data(self) -> dict`
+Devuelve un diccionario con todas las cotizaciones de los diferentes tipos de dólares.
+
+#### `get_blue_price(self) -> dict`
+Devuelve un diccionario con la cotización del dólar blue.
+
+#### `get_oficial_price(self) -> dict`
+Devuelve un diccionario con la cotización del dólar oficial.
+
+#### `get_bolsa_price(self) -> dict`
+Devuelve un diccionario con la cotización del dólar bolsa.
+
+#### `get_contado_con_liqui_price(self) -> dict`
+Devuelve un diccionario con la cotización del dólar contado con liqui.
+
+#### `get_tarjeta_price(self) -> dict`
+Devuelve un diccionario con la cotización del dólar tarjeta.
+
+#### `get_price(self, tipo_dolar: TipoDolar, tipo_operacion: TipoOperacion) -> float`
+Devuelve el precio de compra o venta para un tipo de dólar y operación específicos.
+
+#### `get_all_prices(self, tipo_operacion: TipoOperacion) -> dict`
+Devuelve un diccionario con todos los precios de compra o venta para todos los tipos de dólares.
+
+#### `get_summary(self) -> str`
+Devuelve un resumen en formato de cadena con todas las cotizaciones de los diferentes tipos de dólares, incluyendo los precios de compra y venta, la fuente y la URL de la fuente.
+"""
+
 from enum import Enum
 import requests
 from bs4 import BeautifulSoup
